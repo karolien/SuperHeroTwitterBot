83f090c3e18944a0feff122093bfbb0f5a50795f
Karolien <kkoo939@aucklanduni.ac.nz>
2015-11-23 20:08:54 +1300

    Changing to reply to mentions

--- SuperHeroBot.py
+++ SuperHeroBot.py
@@ -12,12 +12,17 @@ import os
 conn = MySQLdb.connect(os.environ['SERVER'],os.environ['USER_NAME'],os.environ['PASSWORD'],os.environ['DATABASE_NAME'])
 c = conn.cursor()
 
-
 #consumer key, consumer secret, access token, access secret.
 ckey=os.environ['CONSUMER_KEY']
 csecret=os.environ['CONSUMER_SECRET']
 atoken=os.environ['ACCESS_TOKEN']
 asecret=os.environ['ACCESS_SECRET']
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+api = tweepy.API(auth)
+api.verify_credentials
 
 class listener(StreamListener):
 
@@ -27,21 +32,25 @@ class listener(StreamListener):
         tweet = all_data["text"]
         
         username = all_data["user"]["screen_name"]
-        
-        c.execute("INSERT INTO superheronames (id, superheroname, username) VALUES (null,%s,%s)",
-            ("bob brown", username))
+		id = all_data["user"]["id"]
+		superheroname = ""
+		tweet = ""
+		try:
+			c.execute("SELECT superheroname FROM superheronames WHERE user_id = " + id)
+			superheroname = c.fetchone()
+			tweet = ("Hello " + superheroname + " @" + username)
+			api.update_status(tweet)
+		except:
+			superheroname = "Super Man"
+			c.execute("INSERT INTO superheronames (user_id, superheroname) VALUES (%d,%s)",
+				(id, superheroname))
+			conn.commit()
+			tweet = ("Welcome to the club " + superheroname + " @" + username)
 
-        conn.commit()
-
-        print((username,tweet))
-        
         return True
 
     def on_error(self, status):
         print status
-
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
-
+		
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["car"])
+twitterStream.filter(track=["@aSuperHeroClub"])
