e5acd67b34e91a6299ccac67aeabc9a5c02167f8
Karolien <kkoo939@aucklanduni.ac.nz>
2016-03-18 22:01:15 +1300

    Editing WordsApi declaration

--- SuperHeroBot.py
+++ SuperHeroBot.py
@@ -29,7 +29,7 @@ auth.set_access_token(atoken, asecret)
 api = tweepy.API(auth)
 api.verify_credentials
 
-wordApi = WordApi.WordsApi(client)
+wordApi = WordsApi.WordsApi(client)
 
 class listener(StreamListener):
 	def on_data(self,data):
