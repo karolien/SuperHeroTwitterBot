14b4b0ecebe986beafc62f31d14e43849141532d
Alan Dragicevich <aldragicevich@gmail.com>
2016-03-19 11:29:37 +1300

    MIssing import Urllib2

--- SuperHeroBot.py
+++ SuperHeroBot.py
@@ -7,6 +7,7 @@ import MySQLdb
 import time
 import json
 import os
+import urllib2
 
 #wordnik connection
 wordnikURL = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=" + str(['WORDNIK_KEY']);
