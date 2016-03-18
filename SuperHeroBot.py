631b265662ceae419feb876896f9cfe5822f5860
Karolien <kkoo939@aucklanduni.ac.nz>
2016-03-18 20:35:32 +1300

    First attempt at Wordnik API

--- SuperHeroBot.py
+++ SuperHeroBot.py
@@ -1,12 +1,17 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+from wordnik import *
 import tweepy
 import MySQLdb
 import time
 import json
 import os
 
+#wordnik connection
+apiUrl = 'http://api.wordnik.com/v4'
+apiKey = ['WORDNIK_KEY']
+client = swagger.ApiClient(apiKey, apiUrl)
 #                        server       MySQL username	MySQL pass  Database name.
 
 conn = MySQLdb.connect(os.environ['SERVER'],os.environ['USER_NAME'],os.environ['PASSWORD'],os.environ['DATABASE_NAME'])
@@ -24,6 +29,8 @@ auth.set_access_token(atoken, asecret)
 api = tweepy.API(auth)
 api.verify_credentials
 
+wordApi = WordApi.WordApi(client)
+
 class listener(StreamListener):
 	def on_data(self,data):
 		all_data = json.loads(data)
@@ -33,22 +40,15 @@ class listener(StreamListener):
 		id = int(id)
 		superheroname = ""
 		tweet = "" 
-
+		print(getNoun)
 		try:
-			print ("entered try statement")
 			sql = ("SELECT superheroname FROM superheronames WHERE user_id = " + str(id))
-			print(sql)
 			c.execute(sql)
-			print ("executed SELECT statement")
 			superheroname = c.fetchone()[0]
-			print ("got superheroname = " + superheroname)
 			tweet = ("Hello " + superheroname + " @" + username)
-			print ("tweeting")
 			api.update_status(status=tweet)
-			print ("success")
 		except:
-			superheroname = "Super Man"
-			print("exception raised!")
+			superheroname = getNoun
 			c.execute("INSERT INTO superheronames (user_id, superheroname) VALUES (%s,%s)",
 				(id, superheroname))
 			conn.commit()
@@ -61,3 +61,8 @@ class listener(StreamListener):
 		
 twitterStream = Stream(auth, listener())
 twitterStream.filter(track=["@aSuperHeroClub"])
+
+def getNoun():
+	noun = wordApi.getNoun()
+	return noun
+				  
