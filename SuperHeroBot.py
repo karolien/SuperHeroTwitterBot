f3fa5766be7363daa5d438403a9a6c72e9f1b732
Karolien <kkoo939@aucklanduni.ac.nz>
2016-03-18 17:54:24 +1300

    Removed pymysql module from imports

--- SuperHeroBot.py
+++ SuperHeroBot.py
@@ -6,7 +6,6 @@ import MySQLdb
 import time
 import json
 import os
-import pymysql
 #        replace mysql.server with "localhost" if you are running via your own server!
 #                        server       MySQL username	MySQL pass  Database name.
 
